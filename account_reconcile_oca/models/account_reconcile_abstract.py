f2b94958c66dd24c206adeb81dbb72f7cae43301
anjeel.haria <anjeel.haria@brainvire.com>
2024-01-11 16:20:51 +0530

    Fix for reviewing transactions involving exchange differences

--- account_reconcile_oca/models/account_reconcile_abstract.py
+++ account_reconcile_oca/models/account_reconcile_abstract.py
@@ -36,6 +36,7 @@ class AccountReconcileAbstract(models.AbstractModel):
     def _get_reconcile_line(
         self, line, kind, is_counterpart=False, max_amount=False, from_unreconcile=False
     ):
+        account_move_line_obj = self.env['account.move.line']
         date = self.date if "date" in self._fields else line.date
         original_amount = amount = net_amount = line.debit - line.credit
         amount_currency = self.company_id.currency_id
@@ -87,8 +88,11 @@ class AccountReconcileAbstract(models.AbstractModel):
                 {
                     "id": False,
                     "counterpart_line_id": (
-                        line.matched_debit_ids.mapped("debit_move_id")
-                        | line.matched_credit_ids.mapped("credit_move_id")
+                            (line.matched_debit_ids and line.matched_debit_ids[0].mapped(
+                                "debit_move_id") or account_move_line_obj)
+                            | (line.matched_credit_ids and line.matched_credit_ids[0].mapped(
+                        "credit_move_id") or account_move_line_obj
+                               )
                     ).id,
                 }
             )
