46cfb52a7a533aeff7fa82bfe798423a451e4bad
Anjeel <bytemeasap@gmail.com>
2024-02-23 17:28:42 +0530

    Changes in account_reconcile_oca to open bank statement with no filters and added module project_parent

--- account_reconcile_oca/models/account_journal.py
+++ account_reconcile_oca/models/account_journal.py
@@ -23,6 +23,15 @@ class AccountJournal(models.Model):
         # opem reconcile window for bank and cash journals
         if self.type in ("bank", "cash") and not self._context.get("action_name"):
             self.ensure_one()
-            return self.env["ir.actions.act_window"]._for_xml_id(
+            action = self.env["ir.actions.act_window"]._for_xml_id(
                 "account_reconcile_oca.action_bank_statement_line_reconcile")
+            action["context"] = self._context.copy()
+            action["context"].update({
+                "default_journal_id": self.id,
+                "view_ref": "account_reconcile_oca.bank_statement_line_form_reconcile_view"
+            })
+            action["view_mode"] = "list,kanban"
+            action["views"] = [(False, "list"), (
+            self.env.ref("account_reconcile_oca.bank_statement_line_reconcile_view").id, "kanban"), ]
+            return action
         return super().open_action()
