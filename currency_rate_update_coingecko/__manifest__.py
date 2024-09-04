5130977ce8ad707c779047b58cf14680c9c48ca5
Anjeel Haria <bytemeasap@gmail.com>
2024-09-04 17:06:17 +0530

    [ADD] currency_rate_update_coingecko and currency_rate_update_mapping modules
    Added crypto_currency module and pre-commit fixes

---
+++ currency_rate_update_coingecko/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Tecnativa - Ernesto Tejeda
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Currency Rate Update: CoinGecko",
+    "version": "16.0.1.0.0",
+    "category": "Financial Management/Configuration",
+    "summary": "Update exchange rates using CoinGecko",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/currency",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": ["currency_rate_update_mapping", "crypto_currency"],
+    "external_dependencies": {"python": ["pycoingecko"]},
+}
