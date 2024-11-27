18e2931a21a7502c48322d33a7d0ee6238a4f18f
Anjeel Haria <bytemeasap@gmail.com>
2024-11-27 15:36:04 +0530

    [ADD] Added mail_server_user module
    Added field to writeable fields

---
+++ mail_server_user/models/res_users.py
@@ -0,0 +1,25 @@
+# Copyright 2024 Onestein B.V. (https://forgeflow.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    ir_mail_server_id = fields.Many2one(
+        "ir.mail_server",
+        "Outgoing Email Server",
+        help="The email server with least Priority would be used.Make sure the From filtering is set to your email",
+    )
+
+    @property
+    def SELF_READABLE_FIELDS(self):
+        return super().SELF_READABLE_FIELDS + [
+            "ir_mail_server_id",
+        ]
+
+    @property
+    def SELF_WRITEABLE_FIELDS(self):
+        return super().SELF_WRITEABLE_FIELDS + [
+            "ir_mail_server_id",
+        ]
