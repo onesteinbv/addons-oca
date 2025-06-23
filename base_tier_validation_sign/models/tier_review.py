55226f995875c1e92b5e9d9c17496c905e343563
Anjeel Haria <bytemeasap@gmail.com>
2025-06-23 20:24:23 +0530

    [MIG] base_tier_validation_sign: Migration to 18.0

---
+++ base_tier_validation_sign/models/tier_review.py
@@ -0,0 +1,12 @@
+# Copyright 2024 Onestein
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class TierReview(models.Model):
+    _inherit = "tier.review"
+
+    signature = fields.Image(help="Signature", attachment=True)
+    requires_signature = fields.Boolean(
+        related="definition_id.requires_signature", readonly=True
+    )
